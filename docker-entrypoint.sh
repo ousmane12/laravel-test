58771a5a49f6d309d1fb0d0d487f706d21e53fb5
Ousmane Elhadji <36844621+ousmane12@users.noreply.github.com>
2024-08-28 20:34:25 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-set -e
 
-# Execute migrations
+# Execute migrations and seeds
+composer install
 php artisan migrate --force
-
-# Seed database
 php artisan db:seed --force
+
+# Check and generate app key
+if [ ! -f .env ]; then
+  cp .env.example .env
+fi
+php artisan key:generate
+
+# Clean cache
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:clear
 
 # Launch PHP server
 php-fpm
